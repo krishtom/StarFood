7c527097de662b1bf422b6f215f497705e12a3f6
Deepak Kumar Mahto <kumar1737deepak@gmail.com>
2024-12-25 18:14:55 +0530

    food message add

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-content-left">
             <img className='footer-logo' src={assets.logo} alt="" />
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <p>We’re passionate about bringing delicious meals straight to your door. Whether you're craving comfort food or exploring new cuisines, we’ve got you covered.</p>
             <div className="footer-social-icons">
                 <img src={assets.facebook_icon} alt="" />
                 <img src={assets.twitter_icon} alt="" />
@@ -33,13 +33,13 @@ const Footer = () => {
         <div className="footer-content-right">
             <h2>GET IN TOUCH</h2>
             <ul>
-                <li>+1-212-456-7890</li>
-                <li>contact@tomato.com</li>
+                <li>+91-9939159941</li>
+                <li>contact@starfood.com</li>
             </ul>
         </div>
       </div>
       <hr />
-      <p className="footer-copyright">Copyright 2024 © Tomato.com - All Right Reserved.</p>
+      <p className="footer-copyright">Copyright 2024 © StarFood.com - All Right Reserved.</p>
     </div>
   )
 }
