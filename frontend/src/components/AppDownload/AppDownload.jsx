061b11d343e73e045397e48c18aead8672e836b1
Deepak Kumar Mahto <kumar1737deepak@gmail.com>
2024-12-25 18:00:39 +0530

    about page add

---
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,147 @@
+import React from 'react'
+import './AppDownload.css'
+import { assets } from '../../assets/assets'
+
+const AppDownload = () => {
+    return (
+        <div className='aboutus' id='aboutus'>
+             <div className="container">
+      <div className="row p-5 mt-5 border-top">
+        <h1 className=" text-center">People</h1>
+      </div>
+
+      <div className="row p-5 text-muted">
+        <div className="col-6 px-5 text-center  ">
+          <img
+            style={{ width: "15em", borderRadius: "100%" }}
+            src="media/images/jyotipriya.png"
+          />
+          <div className="mr-40">
+          <h4 className="mt-6 mr-14">Jyoti Priya</h4>
+          <h6 className="mr-14">Backend Developer</h6>
+          </div>
+        </div>
+        <div className="col-6 ">
+          <p>
+            I am Jyoti Priya, a passionate Full Stack Developer currently
+            pursuing my Master of Computer Applications (MCA) (2023–25).
+          </p>
+
+          <p>
+            I have a strong foundation in web development, with expertise in
+            Backend Development with Mongodb,Node.js,ExpressJs,SQL and I Have
+            also knowledge about HTML, CSS, JavaScript.
+          </p>
+
+          <p>
+            My focus lies in creating user-friendly, dynamic web applications by
+            combining responsive frontends with robust backends. Through
+            academic and personal projects, I have gained hands-on experience in
+            building RESTful APIs, designing efficient database schemas, and
+            crafting seamless user experiences.
+          </p>
+        </div>
+      </div>
+
+      <div className="row p-5 text-muted">
+        <div className="col-6 px-5 text-center  ">
+          <img
+            style={{ width: "15em", borderRadius: "100%" }}
+            src="media/images/DeepakKumarMahto.jpg"
+          />
+          <div className="mr-40">
+          <h4 className="mt-6 mr-14">Deepak Kumar Mahto</h4>
+          <h6 className="mr-14">Frontend Developer (React) & Designing </h6>
+          </div>
+        </div>
+        <div className="col-6 ">
+          <p>
+            I am Deepak Kumar Mahto, a passionate Frontend Developer (React)
+            currently pursuing my Master of Computer Applications (MCA)
+            (2023–25).
+          </p>
+
+          <p>
+            I have a strong foundation in web development, with expertise in
+            HTML, CSS, JavaScript, React.js, and knowledge about Node.js,
+            MongoDB, and SQL.
+          </p>
+
+          <p>
+            My focus lies in creating user-friendly, dynamic web applications by
+            combining responsive frontends with robust backends. Through
+            academic and personal projects, I have gained hands-on experience in
+            building RESTful APIs, designing efficient database schemas, and
+            crafting seamless user experiences.
+          </p>
+        </div>
+      </div>
+
+      <div className="row p-5 text-muted">
+        <div className="col-6 px-5 text-center  ">
+          <img
+            style={{ width: "15em", borderRadius: "100%" }}
+            src="media/images/monikakumari.png"
+          />
+          <div className="mr-40">
+          <h4 className="mt-6 mr-14">Monika Kumari</h4>
+          <h6 className="mr-14">Backend Developer</h6>
+          </div>
+        </div>
+        <div className="col-6 ">
+          <p>
+            I am Monika Kumari, a passionate Full Stack Developer currently
+            pursuing my Master of Computer Applications (MCA) (2023–25).
+          </p>
+
+          <p>
+            I have a strong foundation in web development, with expertise in
+            Backend Development with Mongodb,Node.js,ExpressJs,SQL and I Have
+            also knowledge about HTML, CSS, JavaScript.
+          </p>
+
+          <p>
+            My focus lies in creating user-friendly, dynamic web applications by
+            combining responsive frontends with robust backends. Through
+            academic and personal projects, I have gained hands-on experience in
+            building RESTful APIs, designing efficient database schemas, and
+            crafting seamless user experiences.
+          </p>
+        </div>
+      </div>
+
+      <div className="row p-5 text-muted">
+        <div className="col-6 px-5 text-center  ">
+          <img
+            style={{ width: "15em", borderRadius: "100%" }}
+            src="media/images/richakumari.jpg"
+          />
+          <div className="mr-40">
+          <h4 className="mt-6 mr-14">Richa Kumari</h4>
+          <h6 className="mr-14">Frontend Developer</h6>
+          </div>
+        </div>
+        <div className="col-6 ">
+          <p>
+            I am Richa kumari, a passionate Frontend Developer currently
+            pursuing my Master of Computer Applications (MCA) (2023–25).
+          </p>
+
+          <p>
+            I have a strong foundation in web development, with expertise in
+            HTML, CSS, JavaScript.
+          </p>
+
+          <p>
+            My focus lies in creating user-friendly, dynamic web applications by
+            combining responsive frontends. Through academic and personal
+            projects, I have gained hands-on experience in Frontend Deve.
+          </p>
+        </div>
+      </div>
+    </div>
+        </div>
+    )
+}
+
+export default AppDownload
